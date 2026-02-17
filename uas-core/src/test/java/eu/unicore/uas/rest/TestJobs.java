73a7c4cb9c4ee5e8467fd9581010bde56ff836e7
Bernd Schuller <b.schuller@fz-juelich.de>
2026-02-17 13:28:13 +0100

    add port-forwarding test

--- uas-core/src/test/java/eu/unicore/uas/rest/TestJobs.java
+++ uas-core/src/test/java/eu/unicore/uas/rest/TestJobs.java
@@ -3,9 +3,22 @@ package eu.unicore.uas.rest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import eu.unicore.client.Endpoint;
@@ -13,9 +26,12 @@ import eu.unicore.client.core.CoreClient;
 import eu.unicore.client.core.JobClient;
 import eu.unicore.client.core.StorageClient;
 import eu.unicore.services.restclient.BaseClient;
+import eu.unicore.services.restclient.ForwardingHelper;
 import eu.unicore.services.restclient.IAuthCallback;
 import eu.unicore.services.restclient.UsernamePassword;
 import eu.unicore.uas.SecuredBase;
+import eu.unicore.uas.util.EchoServer;
+import eu.unicore.util.ChannelUtils;
 
 public class TestJobs extends SecuredBase {
 
@@ -117,10 +133,79 @@ public class TestJobs extends SecuredBase {
 		Thread.sleep(500);
 	}
 
+	@Test
+	public void testPortForwarding() throws Exception {
+		String url = kernel.getContainerProperties().getContainerURL()+"/rest/core/jobs";
+		System.out.println("Accessing "+url);
+		final IAuthCallback auth = new UsernamePassword("demouser", "test123");
+		final BaseClient client = new BaseClient(url, kernel.getClientConfiguration(), auth);
+		JSONObject task = new JSONObject();
+		task.put("ApplicationName", "Date");
+		String jobUrl = client.create(task);
+		System.out.println("created: "+jobUrl);
+		JobClient job = new JobClient(new Endpoint(jobUrl), kernel.getClientConfiguration(), auth);
+		job.poll(null);
+		System.out.println("Job is: "+job.getStatus()+", starting port forwarding.");
+		String forwardURL = job.getLinkUrl("forwarding")+"?port="+echo.getServerPort();
+		client.setURL(forwardURL);
+		ForwardingHelper fh = new ForwardingHelper(client);
+		final AtomicInteger localPort = new AtomicInteger();
+		Thread t = new Thread( ()->{
+			try {
+				try(ServerSocketChannel sc = ServerSocketChannel.open()){
+					sc.configureBlocking(false);
+					sc.bind(new InetSocketAddress(InetAddress.getLocalHost(), 0), 1);
+					fh.accept(sc, (c)->{
+						try {
+							SocketChannel serviceProxy = fh.connect(forwardURL);
+							fh.startForwarding(c, serviceProxy);
+						}catch(Exception ex) {
+							throw new RuntimeException(ex);
+						}
+					});
+					localPort.set(((InetSocketAddress)(sc.getLocalAddress())).getPort());
+					fh.run();
+				}
+			}
+			catch(Exception e) {}
+		});
+		t.start();
+		while(localPort.get()==0)Thread.sleep(200);
+		try(SocketChannel s = SocketChannel.open(new InetSocketAddress(InetAddress.getLocalHost(), localPort.get()))){
+			PrintWriter w = new PrintWriter(new OutputStreamWriter(ChannelUtils.newOutputStream(s, 65536)), true);
+			Reader r = new InputStreamReader(ChannelUtils.newInputStream(s, 65536));
+			BufferedReader br = new BufferedReader(r);
+			System.out.println("Forwarding established");
+			String line = "this is a test";
+			System.out.println("--> "+line);
+			w.println(line);
+			String reply = br.readLine();
+			System.out.println("<-- "+reply);
+			assertEquals(line, reply);
+		}
+	}
+
 	private boolean contains(JSONArray array, Object o) throws JSONException {
 		for(int i=0; i<array.length();i++){
 			if(array.get(i).equals(o))return true;
 		}
 		return false;
 	}
+
+
+	private static EchoServer echo = null;
+
+	@BeforeAll
+	public static void startEchoServer() throws Exception {
+		echo = new EchoServer();
+		echo.start();
+	}
+
+	@AfterAll
+	public static void stopEchoServer() {
+		if(echo!=null)try{
+			echo.shutdown();
+		}catch(Exception e) {}
+	}
+
 }
